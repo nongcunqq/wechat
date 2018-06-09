f3765833216fcec4423e7a931057da30f6e40924
nongcunqq <40048329+nongcunqq@users.noreply.github.com>
2018-06-09 10:12:07 +0800

    Create 将字符串转换为字典.py

---
+++ 将字符串转换为字典.py
@@ -0,0 +1,34 @@
+
+def headers_to_dict(headers):
+    """
+    将字符串
+    '''
+    Host: mp.weixin.qq.com
+    Connection: keep-alive
+    Cache-Control: max-age=
+    '''
+    转换成字典对象
+    {
+        "Host": "mp.weixin.qq.com",
+        "Connection": "keep-alive",
+        "Cache-Control":"max-age="
+    }
+    :param headers: str
+    :return: dict
+    """
+    headers = headers.split("\n")
+    d_headers = dict()
+    for h in headers:
+        if h:
+            k, v = h.split(":", 1)
+            d_headers[k] = v.strip()
+    return d_headers
+
+
+
+a = '''Host: mp.weixin.qq.com
+       Connection: keep-alive'''
+
+a = headers_to_dict(a)
+
+print(a)
