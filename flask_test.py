e5af6ff8a1faa126f5106b0626648d583120987b
nongcunqq <40048329+nongcunqq@users.noreply.github.com>
2019-01-06 18:12:24 +0800

    Update flask_test.py
    增加随机数，用来证明每次显示最新查询结果

--- flask_test.py
+++ flask_test.py
@@ -1,22 +1,12 @@
 from flask import Flask
-import requests, re, hashlib
+import requests, re, hashlib,random
 from bs4 import BeautifulSoup
 import library_login
 
-l = ['taea0015565', 'taea0013896', 'taea0013798', 'taea0018776', 'taea0013797']  # 空港图书馆卡号
-lit = ''
-for i in l:
-
-    card = i.upper()
-    password = library_login.md5(str(i[-6:]))
-
-    a,b = library_login.get_info(card=card, password=password)
-
-    lit = lit + a + '\n' + b + '\n'
-
-lit = lit.replace('\n','<br/>').replace('(','').replace(')','').replace("'","").replace(',','&nbsp;')
 
 
+# b = random.randint(1,120)
+# b = str(b)
 
 
 
@@ -24,10 +14,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    l = ['taea0015565', 'taea0013896', 'taea0013798', 'taea0018776', 'taea0013797']  # 空港图书馆卡号
+    lit = ''
+    for i in l:
+        card = i.upper()
+        password = library_login.md5(str(i[-6:]))
+
+        a, b = library_login.get_info(card=card, password=password)
+
+        lit = lit + a + '\n' + b + '\n'
+
+    lit = lit.replace('\n', '<br/>').replace('(', '').replace(')', '').replace("'", "").replace(',', '&nbsp;')
+    b = random.randint(1, 120)
+    b = str(b)
+    lit = '随机数： ' + b + '<br/>' + lit
 
     return lit
 
 
 
 if __name__ == '__main__':
-    app.run(port = 8000,debug = True)
+    app.run(host = '0.0.0.0', port = 8000,debug = True)
